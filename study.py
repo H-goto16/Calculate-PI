21fba6dcab0f60e86addbaf1cc797679a443ea62
Haruki Goto <97876815+H-goto16@users.noreply.github.com>
2022-04-24 17:28:28 +0900

    Add files via upload

---
+++ study.py
@@ -0,0 +1,15 @@
+from asyncio.windows_events import INFINITE
+
+
+n = 1
+sum = 0
+
+def Leibniz(n):
+    return 4*(( -1 ) ** n / ( 2*n + 1 ))
+time = input("試行回数を入力")
+
+for i in range(0,int(time)):
+    sum += Leibniz(i)
+print('{:.30f}'.format(sum))  
+
+input("input some key")
